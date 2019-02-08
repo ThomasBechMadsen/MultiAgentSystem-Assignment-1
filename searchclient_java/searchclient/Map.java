73e0d801e9b251dd9b441025bfcf39ac3560a7ec
Sebastian Hjorth <Hjorthensebastian@gmail.com>
2019-02-08 15:44:26 +0100

    Merged tile and walls into same array, making each state take up half memory

--- searchclient_java/searchclient/Map.java
+++ searchclient_java/searchclient/Map.java
@@ -3,47 +3,47 @@ package searchclient;
 public class Map {
     public int max_row;// = 70;
     public int max_col;// = 70;
-    private boolean[][] walls;
-    private char[][] goals;
+    // Size goals can only be 'a'..'z' we make use of 0xFF for walls
+    private char[][] tile;
+    private final char kWall = 0xFF;
     
     public Map(int rows, int cols) {
     	max_row = rows;
     	max_col = cols;
-    	walls = new boolean[max_row][max_col];
-    	goals = new char[max_row][max_col];
+    	tile = new char[max_row][max_col];
     }
     
     public void setWall(int row, int col)
     {
-    	walls[row][col] = true;
+    	tile[row][col] = kWall;
     }
     
     public boolean hasWall(int row, int col)
     {
-    	return walls[row][col];
+    	return tile[row][col] == kWall;
     }
     
     public char getGoal(int row, int col)
     {
-    	return goals[row][col];
+    	return tile[row][col];
     }
     
     public void setGoal(int row, int col, char c)
     {
-    	goals[row][col] = c;
+    	tile[row][col] = c;
     }
     
     public StringBuilder GetMapLayout() {
     	StringBuilder s = new StringBuilder();
     	for (int row = 0; row < max_row; row++) {
-            if (!walls[row][0]) {
+            if (!hasWall(row, 0)) {
                 break;
             }
             for (int col = 0; col < max_col; col++) {
                 //TODO: Write boxes
-            	if (goals[row][col] > 0) {
-                    s.append(goals[row][col]);
-                } else if (walls[row][col]) {
+            	if (getGoal(row, col) >= 'a' && getGoal(row, col) <= 'z') {
+                    s.append(getGoal(row, col));
+                } else if (hasWall(row, col)) {
                     s.append("+");
                 } else {
                     s.append(" ");
